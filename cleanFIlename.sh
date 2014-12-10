044898078feb35b002b9a7ddb39bf84dc1d92251
xgdgsc <xgdgsc@gmail.com>
2014-12-10 21:40:45 +0800

    add filtering

--- cleanFIlename.sh
+++ cleanFIlename.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
 for x in `find assets -type f`; do mv "$x" `echo "$x" | sed s/:/__/g` ; done
+for x in `find assets -type d`; do mv "$x" `echo "$x" | sed s/:/__/g` ; done
+rm assets/en/FiSH.html
+rm assets/en/Fish.html
+rm assets/en/Vim/.vimrc.html
